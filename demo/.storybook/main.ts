ccfeca682b0ab7a0859899e3a7a3f369839b1d89
Jules Blom <julesblom@gmail.com>
2025-06-01 13:04:05 +0200

    Storybook path fix

--- demo/.storybook/main.ts
+++ demo/.storybook/main.ts
@@ -1,26 +1,34 @@
-import type { StorybookConfig } from '@storybook/react-vite';
+import type { StorybookConfig } from "@storybook/react-vite";
+import { mergeConfig } from "vite";
 
 const config: StorybookConfig = {
-  "stories": [
+  stories: [
     "../stories/**/*.mdx",
-    "../stories/**/*.stories.@(js|jsx|mjs|ts|tsx)"
+    "../stories/**/*.stories.@(js|jsx|mjs|ts|tsx)",
   ],
-  "addons": [
+  addons: [
     "@storybook/addon-onboarding",
     "@chromatic-com/storybook",
     "@storybook/addon-docs",
     "@storybook/addon-a11y",
-    "@storybook/addon-vitest"
+    "@storybook/addon-vitest",
   ],
-  "framework": {
-    "name": "@storybook/react-vite",
-    "options": {}
+  framework: {
+    name: "@storybook/react-vite",
+    options: {},
   },
-  "viteFinal": async (config) => {
-    return {
-      ...config,
-      base: '/react-windrose/',
-    };
+  managerHead: (head, { configType }) => {
+    if (configType === 'PRODUCTION') {
+      return (`
+        <base href="/react-windrose/">
+        ${head}
+      `);
+    }
+  },
+  viteFinal: async (config) => {
+    return mergeConfig(config, {
+      base: "/react-windrose/",
+    });
   },
 };
 export default config;
